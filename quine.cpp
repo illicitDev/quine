6d1924800e43b3eda409e733ff0d825148fd387d
Austin Francis <afrancis2345@gmail.com>
2020-07-11 11:01:44 -0700

    done

---
+++ quine.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	// store source
+	std::string data = "std::string data = 'std::cout << data << std::endl;'";
+	// print source
+	std::cout << data << std::endl;
+}
