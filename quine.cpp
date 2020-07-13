c0b4e25646626cf1e03b6c98c35a1c6aa04d78df
Austin Francis <afrancis2345@gmail.com>
2020-07-13 10:34:04 -0700

    done

--- quine.cpp
+++ quine.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 
+const char q = 34;
+
+std::string data[] = {
+"	#include <iostream> ",
+
+"	const char q = 34; ", 
+
+"	std::string data[] = { ",
+"	}; ",
+
+"	int main() { ", 
+"		for (int i=0; i<3; i++) ",
+"			std::cout << data[i] << std::endl; ",
+"		for (int j=0; j<17; j++) { ",
+"			std::cout << q + data[j] + q; ",
+"			if (j < 16) ", 
+"				std::cout << ',' << std::endl; ",
+"			else ", 
+"				std::cout << data[3] << std::endl;", 
+"		} ",  
+"		for (int k=4; k<17; k++) ",
+"			std::cout << data[k] << std::endl; ",
+"	}"
+};
+
 int main() {
-	// store source
-	std::string data = "std::string data = 'std::cout << data << std::endl;'";
-	// print source
-	std::cout << data << std::endl;
+	for (int i=0; i<3; i++) 
+		std::cout << data[i] << std::endl;
+	for (int j=0; j<17; j++) { 
+		std::cout << q + data[j] + q;
+		if (j < 16)
+			std::cout << ',' << std::endl;
+		else
+			std::cout << data[3] << std::endl;
+	}
+	for (int k=4; k<17; k++) 
+		std::cout << data[k] << std::endl;
 }
